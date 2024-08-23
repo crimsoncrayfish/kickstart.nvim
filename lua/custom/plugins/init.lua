9dc841afb61ccfd54e1fbe898c0b8b7491e690e3
marco.steenkamp <marco.steenkamp@standardbank.co.za>
2024-08-23 07:40:43 +0200

    updated neotree for hidden files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,30 @@
 -- See the kickstart.nvim README for more information
 return {
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- '.git',
+            -- '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
+    },
+  },
 }
